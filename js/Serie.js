bf4647c32058e45a7557b67df49e7a2a18baab61
Juanes2424 <55508909+Juanes2424@users.noreply.github.com>
2024-10-19 14:01:01 -0500

    Add files via upload

---
+++ js/Serie.js
@@ -0,0 +1,11 @@
+export class Serie {
+    constructor(id, title, network, seasons, genre, website, imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.network = network;
+        this.seasons = seasons;
+        this.genre = genre;
+        this.website = website;
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,61 @@
+import { series } from "./data.js";
+function populateTable() {
+    const tableBody = document.getElementById("table-body");
+    if (!tableBody)
+        return;
+    let avg = 0;
+    let count = 0;
+    series.forEach((item, index) => {
+        const row = document.createElement("tr");
+        const id = document.createElement("td");
+        id.textContent = (index + 1).toString();
+        row.appendChild(id);
+        const titulo = document.createElement("td");
+        const titleLink = document.createElement("a");
+        titleLink.href = "#";
+        titleLink.textContent = item.title;
+        titleLink.classList.add("show-link");
+        titleLink.setAttribute("data-id", item.id.toString());
+        titulo.appendChild(titleLink);
+        row.appendChild(titulo);
+        const canal = document.createElement("td");
+        canal.textContent = item.network;
+        row.appendChild(canal);
+        const temp = document.createElement("td");
+        temp.textContent = item.seasons.toString();
+        row.appendChild(temp);
+        tableBody.appendChild(row);
+        avg += item.seasons;
+        count++;
+    });
+    let estadistica = document.getElementById("Promedio");
+    if (estadistica) {
+        estadistica.innerText = "Seasons average: " + String(avg / count);
+    }
+}
+function showCard(id) {
+    const show = series.find((s) => s.id === id);
+    if (!show)
+        return;
+    const card = document.getElementById("showCard");
+    const title = document.getElementById("showTitle");
+    const description = document.getElementById("showDescription");
+    const link = document.getElementById("showLink");
+    const image = document.getElementById("showImage");
+    if (card && title && description && link) {
+        title.textContent = show.title;
+        description.textContent = show.genre;
+        link.href = show.website;
+        image.src = show.imageUrl;
+        card.style.display = "block";
+    }
+}
+document.addEventListener("click", function (event) {
+    const target = event.target;
+    if (target.classList.contains("show-link")) {
+        event.preventDefault();
+        const showId = parseInt(target.getAttribute("data-id") || "0", 10);
+        showCard(showId);
+    }
+});
+window.onload = populateTable;
